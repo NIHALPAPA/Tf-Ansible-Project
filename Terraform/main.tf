227939e8969aabff23d8f5f9d120e2e5c7725c78
IΔM-NIHALPAPA <158464580+NIHALPAPA@users.noreply.github.com>
2024-05-25 20:12:39 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_web" {
   name        = "allow_web"
   description = "Allow TLS inbound traffic"
-  vpc_id = aws_vpc.sudha_vpc.id
+  vpc_id = aws_vpc.nihal_vpc.id
   // Ingress rules (inbound traffic)
   ingress {
     description = "HTTP"
@@ -32,13 +32,13 @@ resource "aws_security_group" "allow_web" {
 
 #vpc
 
-resource "aws_vpc" "sudha_vpc" {
+resource "aws_vpc" "nihal_vpc" {
   cidr_block = "10.0.0.0/16" # specify the CIDR block for your VPC
   enable_dns_support = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "sudha-vpc"
+    Name = "nihal-vpc"
   }
 }
 
@@ -46,47 +46,47 @@ resource "aws_vpc" "sudha_vpc" {
 
 #subnets
 
-resource "aws_subnet" "sudha_public_subnet_1" {
-  vpc_id            = aws_vpc.sudha_vpc.id # specify the ID of the existing VPC
+resource "aws_subnet" "nihal_public_subnet_1" {
+  vpc_id            = aws_vpc.nihal_vpc.id # specify the ID of the existing VPC
   cidr_block        = "10.0.1.0/24"  # specify the CIDR block for the subnet
   availability_zone = "us-east-1a"   # specify the availability zone
 
   map_public_ip_on_launch = true  # enable auto-assign public IP addresses
 
   tags = {
-    Name = "sudha_public-subnet-1"
+    Name = "nihal_public-subnet-1"
   }
 }
 
-resource "aws_subnet" "sudha_public_subnet_2" {
-  vpc_id            = aws_vpc.sudha_vpc.id # specify the ID of the existing VPC
+resource "aws_subnet" "nihal_public_subnet_2" {
+  vpc_id            = aws_vpc.nihal_vpc.id # specify the ID of the existing VPC
   cidr_block        = "10.0.2.0/24"  # specify the CIDR block for the subnet
   availability_zone = "us-east-1b"   # specify the availability zone
 
   map_public_ip_on_launch = true  # enable auto-assign public IP addresses
 
   tags = {
-    Name = "sudha_public-subnet-2"
+    Name = "nihal_public-subnet-2"
   }
 }
 
-resource "aws_subnet" "sudha_private_subnet_1" {
-  vpc_id            = aws_vpc.sudha_vpc.id # specify the ID of the existing VPC
+resource "aws_subnet" "nihal_private_subnet_1" {
+  vpc_id            = aws_vpc.nihal_vpc.id # specify the ID of the existing VPC
   cidr_block        = "10.0.3.0/24"  # specify the CIDR block for the subnet
   availability_zone = "us-east-1a"   # specify the availability zone
 
   tags = {
-    Name = "sudha_private-subnet-1"
+    Name = "nihal_private-subnet-1"
   }
 }
 
-resource "aws_subnet" "sudha_private_subnet_2" {
-  vpc_id            = aws_vpc.sudha_vpc.id # specify the ID of the existing VPC
+resource "aws_subnet" "nihal_private_subnet_2" {
+  vpc_id            = aws_vpc.nihal_vpc.id # specify the ID of the existing VPC
   cidr_block        = "10.0.4.0/24"  # specify the CIDR block for the subnet
   availability_zone = "us-east-1b"   # specify the availability zone
 
   tags = {
-    Name = "sudha_private-subnet-2"
+    Name = "nihal_private-subnet-2"
   }
 }
 
@@ -95,64 +95,64 @@ resource "aws_subnet" "sudha_private_subnet_2" {
 #routetable
 
 resource "aws_route_table" "public_route_table_1" {
-  vpc_id = aws_vpc.sudha_vpc.id # reference to the VPC ID
+  vpc_id = aws_vpc.nihal_vpc.id # reference to the VPC ID
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.sudha-igw.id # reference to the Internet Gateway ID
+    gateway_id = aws_internet_gateway.nihal-igw.id # reference to the Internet Gateway ID
   }
 
   tags = {
-    Name = "Sudha-rtb-public1-us-east-1a"
+    Name = "nihal-rtb-public1-us-east-1a"
   }
 }
 
-resource "aws_route_table_association" "rts-sudha-public1" {
-  subnet_id = aws_subnet.sudha_public_subnet_1.id
+resource "aws_route_table_association" "rts-nihal-public1" {
+  subnet_id = aws_subnet.nihal_public_subnet_1.id
   route_table_id = aws_route_table.public_route_table_1.id
 }
 
 resource "aws_route_table" "public_route_table_2" {
-  vpc_id = aws_vpc.sudha_vpc.id # reference to the VPC ID
+  vpc_id = aws_vpc.nihal_vpc.id # reference to the VPC ID
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.sudha-igw.id # reference to the Internet Gateway ID
+    gateway_id = aws_internet_gateway.nihal-igw.id # reference to the Internet Gateway ID
   }
 
   tags = {
-    Name = "Sudha-rtb-public2-us-east-1b"
+    Name = "nihal-rtb-public2-us-east-1b"
   }
 }
 
-resource "aws_route_table_association" "rts-sudha-public2" {
-  subnet_id = aws_subnet.sudha_public_subnet_2.id
+resource "aws_route_table_association" "rts-nihal-public2" {
+  subnet_id = aws_subnet.nihal_public_subnet_2.id
   route_table_id = aws_route_table.public_route_table_2.id
 }
 
 resource "aws_route_table" "private_route_table_1" {
-  vpc_id = aws_vpc.sudha_vpc.id # reference to the VPC ID
+  vpc_id = aws_vpc.nihal_vpc.id # reference to the VPC ID
 
   tags = {
-    Name = "Sudha-rtb-private1-us-east-1a"
+    Name = "nihal-rtb-private1-us-east-1a"
   }
 }
 
-resource "aws_route_table_association" "rts-sudha-private1" {
-  subnet_id = aws_subnet.sudha_private_subnet_1.id
+resource "aws_route_table_association" "rts-nihal-private1" {
+  subnet_id = aws_subnet.nihal_private_subnet_1.id
   route_table_id = aws_route_table.private_route_table_1.id
   }
 
 resource "aws_route_table" "private_route_table_2" {
-  vpc_id = aws_vpc.sudha_vpc.id # reference to the VPC ID
+  vpc_id = aws_vpc.nihal_vpc.id # reference to the VPC ID
 
   tags = {
-    Name = "Sudha-rtb-private2-us-east-1b"
+    Name = "nihal-rtb-private2-us-east-1b"
   }
 }
 
-resource "aws_route_table_association" "rts-sudha-private2" {
-  subnet_id = aws_subnet.sudha_private_subnet_2.id
+resource "aws_route_table_association" "rts-nihal-private2" {
+  subnet_id = aws_subnet.nihal_private_subnet_2.id
   route_table_id = aws_route_table.private_route_table_2.id  
 }
 
@@ -160,10 +160,10 @@ resource "aws_route_table_association" "rts-sudha-private2" {
 
 #internet gateway
 
-resource "aws_internet_gateway" "sudha-igw" {
-  vpc_id = aws_vpc.sudha_vpc.id # reference to the VPC ID
+resource "aws_internet_gateway" "nihal-igw" {
+  vpc_id = aws_vpc.nihal_vpc.id # reference to the VPC ID
   tags = {
-    Name = "sudha-igw"
+    Name = "nihal-igw"
   }
 }
 
@@ -174,7 +174,7 @@ resource "aws_instance" "nihal-terraform" {
   key_name      = "tf_key"
   count = 2
   availability_zone = "us-east-1a"
-  subnet_id     = aws_subnet.sudha_public_subnet_1.id # specify the subnet ID where instances will be launched
+  subnet_id     = aws_subnet.nihal_public_subnet_1.id # specify the subnet ID where instances will be launched
   security_groups = [aws_security_group.allow_web.id] # specify the security group(s) for the instance(s)
 
   tags = {
